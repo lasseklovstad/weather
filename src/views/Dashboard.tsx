f3eb0ee1a04b825777a9e347bb6b63ab12936d9a
Lasse Kløvstad <lasse.klovstad@ciber.no>
2023-05-11 22:08:25 +0200

    Task 2: List of weatcher locations dashboard

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,15 +1,32 @@
-import { Box, Typography } from "@mui/material";
+import { Container, List, ListItem, Typography } from "@mui/material";
+import { WeatherButton } from "./WeatherButton";
+
+const weatcherLocations = [
+  { locationName: "Berlin", temperature: "10ºC" },
+  { locationName: "London", temperature: "15ºC" },
+];
 
 export const Dashboard = () => {
   return (
-    <Box
-      component="main"
-      display="flex"
-      alignItems="center"
-      my={1}
-      flexDirection="column"
-    >
-      <Typography variant="h1">Dashboard</Typography>
-    </Box>
+    <Container component="main" maxWidth="sm" sx={{ my: 2 }}>
+      <Typography variant="h1" mb={2}>
+        Dashboard
+      </Typography>
+      <List>
+        <ListItem disableGutters>
+          <WeatherButton locationName="My location" temperature="22ºC" />
+        </ListItem>
+        {weatcherLocations.map(({ locationName, temperature }) => {
+          return (
+            <ListItem key={locationName} disableGutters>
+              <WeatherButton
+                locationName={locationName}
+                temperature={temperature}
+              />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Container>
   );
 };

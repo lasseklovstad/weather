a65b68869e862e417e682c6745a3f340a7cd3f8d
Lasse Kløvstad <lasse.klovstad@ciber.no>
2023-05-11 21:20:05 +0200

    Task 1: Change to main tag

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -2,7 +2,13 @@ import { Box, Typography } from "@mui/material";
 
 export const Dashboard = () => {
   return (
-    <Box display="flex" alignItems="center" my={1} flexDirection="column">
+    <Box
+      component="main"
+      display="flex"
+      alignItems="center"
+      my={1}
+      flexDirection="column"
+    >
       <Typography variant="h1">Dashboard</Typography>
     </Box>
   );
